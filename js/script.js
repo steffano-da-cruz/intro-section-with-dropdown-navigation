6dbf550952a7c1b94ff3f0481a67e3853f34d6a0
Steffano da Cruz <steffanodacruz@outlook.com>
2024-08-02 10:06:33 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,11 +2,22 @@
 
 const navBox = document.querySelector(".header-nav-box");
 const menuBox = document.querySelector(".header-menu-box");
-const showMenu = document.querySelector(".snap-header");
+const snapHeader = document.querySelector(".snap-header");
 const navFeatures = document.querySelector(".header-nav-features");
 const navCompany = document.querySelector(".header-nav-company");
 const arrowFeatures = document.querySelector(".features-nav-arrow");
 const arrowCompany = document.querySelector(".company-nav-arrow");
+const overlay = document.querySelector(".overlay");
+
+menuBox.addEventListener("click", function () {
+  if (snapHeader.classList.contains("show-menu")) {
+    snapHeader.classList.remove("show-menu");
+    overlay.classList.add("hidden");
+  } else {
+    snapHeader.classList.add("show-menu");
+    overlay.classList.remove("hidden");
+  }
+});
 
 navFeatures.addEventListener("click", function () {
   if (navBox.classList.contains("show-features")) {
@@ -26,8 +37,4 @@ navCompany.addEventListener("click", function () {
     navBox.classList.add("show-company");
     arrowCompany.src = `./images/icon-arrow-up.svg`;
   }
-});
-
-menuBox.addEventListener("click", function () {
-  showMenu.classList.add("show-menu");
 });
